7e1919a772e93f958a398a2d78242bd9e741deb3
Quentin Guenther <qguenther95@live.com>
2018-01-24 16:47:13 -0800

    Add signup and error routes

--- index.php
+++ index.php
@@ -21,6 +21,19 @@
             echo $template->render('pages/home.html');
         });
 
+    // Sign up route
+    $f3->route('GET /signup',
+       function(){
+           $template = new Template();
+           echo $template->render('pages/personal_information.html');
+       });
+
+    // Error page
+    $f3->set('ONERROR', function($f3) {
+        echo Template::instance()->render('pages/error.html');
+    });
+
+
     // Run fat-free
     $f3->run();
 ?>
