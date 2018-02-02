c506362c631a7e24baa3d03b01e1953302a18171
Quentin Guenther <qguenther95@live.com>
2018-02-01 20:07:10 -0800

    Add validation to the forms

--- index.php
+++ index.php
@@ -14,29 +14,94 @@
     // Set debug level to dev
     $f3->set('DEBUG', 3);
 
+    // Validation script
+    include('models/validation.php');
+
     // Default route
-    $f3->route('GET /',
-        function(){
-            $template = new Template();
-            echo $template->render('pages/home.html');
-        });
+    $f3->route('GET /', function(){
+        echo Template::instance()->render('pages/home.html');
+    });
 
     // Sign up route
-    $f3->route('GET /signup',
-       function($f3){
+    $f3->route('GET|POST /signup/personal_information', function($f3){
+        if(isset($_POST['submit'])) {
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $age = $_POST['age'];
+            $gender = $_POST['gender'];
+            $phoneNumber = $_POST['phoneNumber'];
 
-            $f3->set('indoor', array('tv', 'puzzles', 'movies', 'reading', 'cooking', 'playing cards', 'board games', 'video games'));
-            $f3->set('outdoor', array('hiking', 'walking', 'biking', 'climbing', 'swimming', 'collecting'));
+            $errors = array();
 
-            $template = new Template();
-            echo $template->render('pages/interests.html');
-       });
+            if(!validName($firstName)) { $errors['firstName'] = "First name must contain alphabetic charactors only."; }
+            if(!validName($lastName)) { $errors['lastName'] = "Last name must contain alphabetic charactors only."; }
+            if(!validAge($age)) { $errors['age'] = "You must be atleast 18 years old to join."; }
+            if(!validPhone($phoneNumber)) { $errors['phoneNumber'] = "Must be a valid phone number."; }
 
-    $f3->route('GET /profile',
-       function($f3){
-           $template = new Template();
-           echo $template->render('pages/profile_summary.html');
-       });
+            $errors = array_filter($errors);
+            if(empty($errors)) { 
+                $_SESSION['firstName'] = $firstName;
+                $_SESSION['lastName'] = $lastName;
+                $_SESSION['age'] = $age;
+                $_SESSION['gender'] = $gender;
+                $_SESSION['phoneNumber'] = $phoneNumber;
+                $f3->reroute('/signup/profile'); 
+            }
+
+            $f3->set('firstName', $firstName);
+            $f3->set('lastName', $lastName);
+            $f3->set('age', $age);
+            $f3->set('gender', $gender);
+            $f3->set('phoneNumber', $phoneNumber);
+            $f3->set('errors', $errors);
+        }
+
+        echo Template::instance()->render('pages/personal_information.html');
+    });
+
+    $f3->route('GET|POST /signup/profile', function($f3){
+        if(isset($_POST['submit'])) {
+            $email = $_POST['email'];
+            $state = $_POST['state'];
+            $seekingGender = $_POST['seekingGender'];
+            $biography = $_POST['biography'];
+
+            $f3->set('email', $email);
+            $f3->set('state', $state);
+            $f3->set('seekingGender', $seekingGender);
+            $f3->set('biography', $biography);
+            
+            $f3->reroute('/signup/interests');
+        }
+       
+        
+        echo Template::instance()->render('pages/profile.html');
+        
+    });
+
+    $f3->route('GET|POST /signup/interests', function($f3){
+        if(isset($_POST['submit'])) {
+            $indoor = $_POST['indoor'];
+            $outdoor = $_POST['outdoor'];
+
+            $errors = array();
+
+            if(!validIndoor($outdoor)) { $errors['indoor'] = 'All of your indoor intrests could not be found.'; }
+            if(!validOutdoor($outdoor)) { $errors['outdoor'] = 'All of your outdoor intrests could not be found.'; }
+
+            $errors = array_filter($errors);
+            if(empty($errors)) { $f3->reroute('/profile'); }
+
+            $f3->set('setIndoorIntrests', $indoor);
+            $f3->set('setOutdoorIntrests', $outdoor);
+        }
+
+        echo Template::instance()->render('pages/interests.html');
+    });
+
+    $f3->route('GET /profile', function($f3){
+       echo Template::instance()->render('pages/profile_summary.html');
+    });
 
     // Error page
     $f3->set('ONERROR', function($f3) {
---
+++ models/validation.php
@@ -0,0 +1,114 @@
+<?php
+
+	$indoorIntrests = array('tv', 'puzzles', 'movies', 'reading', 'cooking', 'playing cards', 'board games', 'video games');
+	$outdoorIntrests = array('hiking', 'walking', 'biking', 'climbing', 'swimming', 'collecting');
+	$stateList = array(
+        'ALABAMA',
+        'ALASKA',
+        'AMERICAN SAMOA',
+        'ARIZONA',
+        'ARKANSAS',
+        'CALIFORNIA',
+        'COLORADO',
+        'CONNECTICUT',
+        'DELAWARE',
+        'DISTRICT OF COLUMBIA',
+        'FEDERATED STATES OF MICRONESIA',
+        'FLORIDA',
+        'GEORGIA',
+        'GUAM GU',
+        'HAWAII',
+        'IDAHO',
+        'ILLINOIS',
+        'INDIANA',
+        'IOWA',
+        'KANSAS',
+        'KENTUCKY',
+        'LOUISIANA',
+        'MAINE',
+        'MARSHALL ISLANDS',
+        'MARYLAND',
+        'MASSACHUSETTS',
+        'MICHIGAN',
+        'MINNESOTA',
+        'MISSISSIPPI',
+        'MISSOURI',
+        'MONTANA',
+        'NEBRASKA',
+        'NEVADA',
+        'NEW HAMPSHIRE',
+        'NEW JERSEY',
+        'NEW MEXICO',
+        'NEW YORK',
+        'NORTH CAROLINA',
+        'NORTH DAKOTA',
+        'NORTHERN MARIANA ISLANDS',
+        'OHIO',
+        'OKLAHOMA',
+        'OREGON',
+        'PALAU',
+        'PENNSYLVANIA',
+        'PUERTO RICO',
+        'RHODE ISLAND',
+        'SOUTH CAROLINA',
+        'SOUTH DAKOTA',
+        'TENNESSEE',
+        'TEXAS',
+        'UTAH',
+        'VERMONT',
+        'VIRGIN ISLANDS',
+        'VIRGINIA',
+        'WASHINGTON',
+        'WEST VIRGINIA',
+        'WISCONSIN',
+        'WYOMING',
+        'ARMED FORCES AFRICA \ CANADA \ EUROPE \ MIDDLE EAST',
+        'ARMED FORCES AMERICA (EXCEPT CANADA)',
+        'ARMED FORCES PACIFIC'
+    );
+
+    $f3->set('indoorIntrests', $indoorIntrests);
+    $f3->set('outdoorIntrests', $outdoorIntrests);
+    $f3->set('stateList', $stateList);
+
+	function validName($name)
+	{
+		return ctype_alpha($name);
+	}
+
+	function validAge($age)
+	{
+		return (ctype_digit($age) && $age >= 18);
+	}
+
+	function validPhone($phone)
+	{
+		$regex = "/^(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}$/i";
+		return preg_match($regex, $phone);
+	}
+
+	function validOutdoor($intrests)
+	{
+        if(isset($intrests)) {
+            foreach ($intrests as $key => $value) {
+                if(in_array($value, (array)$indoorIntrests)) {
+                    return false;
+                }
+            }
+        }
+		
+		return true;
+	}
+
+	function validIndoor($intrests)
+	{
+        if(isset($intrests) && !empty($intrests)) {
+            foreach ($intrests as $key => $value) {
+                if(in_array($value, (array)$outdoorIntrests)) {
+                    return false;
+                }
+            }
+        }
+
+		return true;
+	}
